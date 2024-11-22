4936e81733fb6881fac7310ae1351187a751b276
Aliaksandr Artsikhovich <art-orient@tut.by>
2024-11-23 01:16:38 +0300

    add views

--- app/src/main/java/com/art/cameraapp/MainActivity.kt
+++ app/src/main/java/com/art/cameraapp/MainActivity.kt
@@ -7,19 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.art.cameraapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var infoTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         infoTextView = findViewById(R.id.infoTextView)
 
         val buttonCamera = findViewById<Button>(R.id.button_camera)
