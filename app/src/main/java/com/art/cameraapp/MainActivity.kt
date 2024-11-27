e31d74ecfc882c9dbde92231436776d898c95edf
Aliaksandr Artsikhovich <art-orient@tut.by>
2024-11-27 17:40:16 +0300

    camera done

--- app/src/main/java/com/art/cameraapp/MainActivity.kt
+++ app/src/main/java/com/art/cameraapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.art.cameraapp
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -9,30 +10,35 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.art.cameraapp.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var infoTextView: TextView
     private var imageCapture : ImageCapture? = null
+    private lateinit var outputDirectory : File
+    private lateinit var cameraExecutor: ExecutorService
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
         infoTextView = findViewById(R.id.infoTextView)
-
-        val buttonCamera = findViewById<Button>(R.id.button_camera)
-        buttonCamera.setOnClickListener {
-            infoTextView.text = getString(R.string.todo_camera_button)
-
-        }
 
         val buttonFolder = findViewById<Button>(R.id.button_folder)
         buttonFolder.setOnClickListener {
@@ -53,6 +59,45 @@ class MainActivity : AppCompatActivity() {
                 Constants.REQUIRED_PERMISSIONS,
                 Constants.REQUEST_CODE_PERMISSIONS)
         }
+
+        binding.buttonCamera.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun getOutputDirectory() : File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File (outputDirectory, SimpleDateFormat(Constants.FILE_NAME_FORMAT,
+            Locale.getDefault())
+            .format(System.currentTimeMillis()) + ".jpg")
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+                    infoTextView.text = "$msg $savedUri"
+                    Toast.makeText(this@MainActivity, "$msg $savedUri", Toast.LENGTH_LONG)
+                        .show()
+                }
+
+                override fun onError(e: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${e.message}", e)
+                }
+            }
+        )
     }
 
     private fun allPermissionGranted() =
@@ -95,5 +140,10 @@ class MainActivity : AppCompatActivity() {
                 Log.d(Constants.TAG, "startCamera Fail: ", e)
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
