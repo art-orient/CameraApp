e29354aee1dd3e44c9c61973f298084453211189
Aliaksandr Artsikhovich <art-orient@tut.by>
2024-11-27 15:55:35 +0300

    add startCamera

--- app/src/main/java/com/art/cameraapp/MainActivity.kt
+++ app/src/main/java/com/art/cameraapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.art.cameraapp
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.art.cameraapp.databinding.ActivityMainBinding
@@ -14,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var infoTextView: TextView
+    private var imageCapture : ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         
         if (allPermissionGranted()) {
             Toast.makeText(this, "We have Permission", Toast.LENGTH_SHORT).show()
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this,
@@ -54,4 +61,39 @@ class MainActivity : AppCompatActivity() {
                     PackageManager.PERMISSION_GRANTED
         }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build()
+                .also {mPreview -> mPreview.surfaceProvider = binding.viewFinder.surfaceProvider }
+
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+            } catch (e : Exception) {
+                Log.d(Constants.TAG, "startCamera Fail: ", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
 }
